0d7a6c00f2bf4cdd0af128e39babc9b892f48e2c
riccardo.cardiello <riccardo.cardiello@aizoongroup.com>
2022-06-10 11:09:32 +0200

    Fist commit

---
+++ src/main/java/it/academy/ReadFile.java
@@ -0,0 +1,21 @@
+package it.academy;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class ReadFile {
+
+  public static void main(String[] args) throws IOException, URISyntaxException {
+    Path path = Paths.get(ReadFile.class.getClassLoader()
+        .getResource("readMe.txt").toURI());
+
+    try (Stream<String> lines = Files.lines(path)) {
+      lines.forEach(System.out::println);
+    }
+
+  }
+}
