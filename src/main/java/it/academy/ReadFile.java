cd98d1b7ad49d7f206144bf63242e78f6269420d
paul <wistoon321@gmail.com>
2022-06-11 13:13:00 +0200

    Modifica stampa

--- src/main/java/it/academy/ReadFile.java
+++ src/main/java/it/academy/ReadFile.java
@@ -15,7 +15,7 @@ public class ReadFile {
 
     try (Stream<String> lines = Files.lines(path)) {
       lines.forEach(System.out::println);
-      System.out.println("ciao");
+      System.out.println("ciao mondo 123");
     }
 
   }
